14b881cbff5a3beb0fb929a71f29b2bf577a6d91
MaximeBelaid <maxime.belaid@orange.fr>
2015-10-08 18:03:20 +0200

    Doctrine + création du form Création produit

--- src/Troiswa/BackBundle/Controller/ProductController.php
+++ src/Troiswa/BackBundle/Controller/ProductController.php
@@ -10,6 +10,8 @@ namespace Troiswa\BackBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Troiswa\BackBundle\Entity\Product;
 
 class ProductController extends  Controller
 {
@@ -21,5 +23,30 @@ class ProductController extends  Controller
     public function testAction()
     {
         return $this->render("TroiswaBackBundle:Product:test.html.twig");
+    }
+
+    public function createAction(Request $request)
+    {
+        $product = new Product();
+        //$product->setTitle("Hello");
+        $formulaireProduct = $this->createFormBuilder($product)
+                                    ->add("title","text")
+                                    //->add("toto","text") erreur car toto n'existe pas en base
+                                    ->add("description", "textarea")
+                                    ->add("price", "text")
+                                    ->add("quantity", "number")
+                                    ->add("sauvegarder", "submit")
+                                    ->getForm();
+        dump($product);
+        $formulaireProduct->handleRequest($request);
+        if($formulaireProduct->isValid()){
+            $em= $this->getDoctrine()->getManager(); // Récupérer doctrine (se connecter à la base)
+            $em->persist($product); // A partir de ce moment-là Doctrine le surveille
+            $em->flush();
+            //die(dump($product));
+            $this->get("session")->getFlashBag()->add("success_product_create","Le produit a bien été enregistré");
+            return $this->redirectToRoute("troiswa_back_product_create");
+        }
+        return $this->render("TroiswaBackBundle:Product:create.html.twig",["formProduct"=>$formulaireProduct->createView()]);
     }
 }
---
+++ src/Troiswa/BackBundle/Entity/Product.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Troiswa\BackBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="product")
+ * @ORM\Entity(repositoryClass="Troiswa\BackBundle\Entity\ProductRepository")
+ */
+class Product
+{
+    public function __construct()
+    {
+        $this->dateCreated = new \DateTime("now");
+        $this->quantity = 1;
+    }
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Obligatoire")
+     * @Assert\Length(
+     *      min = 2,
+     *      minMessage = "Your title must be at least {{ limit }} characters long",
+     * )
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Obligatoire")
+     * @Assert\Length(
+     *      max = 100,
+     *      maxMessage = "Your description cannot be longer than {{ limit }} characters"
+     * )
+     * @ORM\Column(name="description", type="text",nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var float
+     * @Assert\NotBlank(message="Obligatoire")
+     * @Assert\Regex(
+     *     pattern="/^[0-9]{1,}(,)?[0-9]{1,2}$/",
+     *     message="This value is not valid.")
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_created", type="datetime")
+     */
+    private $dateCreated;
+
+    /**
+     * @var integer
+     * @ORM\Column(name="quantity", type="integer", options={"default"=1})
+     */
+    private $quantity;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Product
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Product
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     *
+     * @return Product
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param \DateTime $dateCreated
+     *
+     * @return Product
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return Product
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
